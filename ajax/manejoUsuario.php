05807b90a70ca68a2694ac773d91653058ee45eb
josigtz <josiavina@gmail.com>
2018-09-22 18:20:26 -0500

    Update manejoUsuario.php

--- ajax/manejoUsuario.php
+++ ajax/manejoUsuario.php
@@ -1,7 +1,26 @@
 <?php
-	//$hola = json_encode(file_get_contents('php://input'));
-	$hola = json_decode(file_get_contents('php://input'), false);
-    error_log($hola->funcion);
+include_once('./class.glosarioDAO.php');
+include_once('./class.glosario.php');
+include_once('./class.usuarioDAO.php');
+include_once('./class.usuario.php');
+include_once('./class.DBPDO.php');
 
-    echo(json_encode(file_get_contents('php://input')));
+if( !isset($_SESSION["bd"]) ){
+    $_SESSION["bd"] = new DBPDO();
+}
+
+$peticion = json_decode(file_get_contents('php://input'), false);
+
+switch ($peticion['funcion']) {
+	case "registro":
+		$DAO = new usuarioDAO();
+		$usuario = new usuario("",$peticion['usuario'],$peticion['nombre'],$peticion['apellidoPaterno'],$peticion['apellidoMaterno'],$peticion['ocupacion'],$peticion['correo'],$peticion['contrasena']);
+		$DAO->createUsuario($usuario);
+	case "login":
+		$DAO = new usuarioDAO();
+		$usuario = new usuario("",$peticion['usuario'],"","","","","",$peticion['contrasena']);
+		var_dump($DAO->readUsuario($usuario));
+	break;
+}
+
 ?>
