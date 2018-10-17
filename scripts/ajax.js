ee9e55d6d145f55cc9560cdb344b71fbc48f2838
vihellor <vihellor@gmail.com>
2018-10-16 20:22:31 -0500

    createIngreso agregado

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -208,23 +208,21 @@ function handleFormSubmit (form,accion) {
             y +='<form id=\'flujo'+jj[i].idFlujo+'\' onsubmit="event.preventDefault(); handleFormSubmit(this,\'ingresos\');"><tr><td><input class="w3-input" form="flujo'+jj[i].idFlujo+'" type="text" name="nombreFlujo" value="'+jj[i].nombreFlujo+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="monto" value="'+jj[i].monto+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="fecha" value="'+jj[i].fechaCorte+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="periodicidad" value="'+jj[i].periodicidad+'"> semanas</td><td><button type="button" onclick="flujoEdit(\'flujo'+jj[i].idFlujo+'\')" name="accion" value="edit"><i class="fa fa-edit fa-fw"></i></button></td><td><button type="button" onclick="flujoDelete(\'flujo'+jj[i].idFlujo+'\')" name="accion" value="delete"><i class="fa fa-times fa-fw"></i></button></td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="hidden" name="idFlujo" value="'+jj[i].idFlujo+'"></tr></form>';
 
           }
-          y+="</tbody>";
+          y+='<tr id="trNewIngreso"><form id="newIngreso" onsubmit="event.preventDefault(); handleFormSubmit(this,"createIngreso");"><td></td><td></td><td><input onclick="showIngreso();" type="button" class="w3-btn w3-green w3-xlarge" value="Nuevo"></td></form></tr></tbody>';
           document.getElementById("tableIngresos").innerHTML=y;
           //console.log(y+"aquí acaba");
         }
         else if(accion.localeCompare("readEgresos")==0){
           var jj = JSON.parse(respuesta);
-          var y;
           var x = document.getElementById("tableEgresos").innerHTML;
           var len= x.length;
-          //console.log(x.slice(0,len-9));
-          y = x.slice(0,len-9);
+          var y = x.slice(0,len-9);
           for (i in jj){
-            //console.log(i);
             y +='<form id=\'flujo'+jj[i].idFlujo+'\' onsubmit="event.preventDefault(); handleFormSubmit(this,\'ingresos\');"><tr><td><input class="w3-input" form="flujo'+jj[i].idFlujo+'" type="text" name="nombreFlujo" value="'+jj[i].nombreFlujo+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="monto" value="'+jj[i].monto+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="fecha" value="'+jj[i].fechaCorte+'"></td><td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="number" name="periodicidad" value="'+jj[i].periodicidad+'"> semanas</td><td><button type="button" onclick="flujoEdit(\'flujo'+jj[i].idFlujo+'\')" name="accion" value="edit"><i class="fa fa-edit fa-fw"></i></button></td><td><button type="button" onclick="flujoDelete(\'flujo'+jj[i].idFlujo+'\')" name="accion" value="delete"><i class="fa fa-times fa-fw"></i></button></td><input form="flujo'+jj[i].idFlujo+'" class="w3-input" type="hidden" name="idFlujo" value="'+jj[i].idFlujo+'"></tr></form>';
-            
+
           }
-          y+="</tbody>";
+          y+='<tr id="trNewEgreso"></tr></tbody>';
+          //console.log(y);
           document.getElementById("tableEgresos").innerHTML=y;
         }else if(accion.localeCompare("readAllUsuario")==0){
           var jj = JSON.parse(respuesta);
@@ -237,6 +235,7 @@ function handleFormSubmit (form,accion) {
             //console.log(i);
             y +='<tr><form id="usr'+jj[i].idUsuario+'" onsubmit="event.preventDefault(); handleFormSubmit(this,\'usr\');"><td><input form="usr'+jj[i].idUsuario+'" class="w3-input" type="text" name="idUsuario" value="'+jj[i].idUsuario+'" disabled="true"></td><td><input form="usr'+jj[i].idUsuario+'" class="w3-input" type="text" name="nickname" value="'+jj[i].nickname+'" disabled="true"></td><td><input form="usr'+jj[i].idUsuario+'" class="w3-input" type="password" name="password"></td><td><button onclick="updateUsr(\'usr'+jj[i].idUsuario+'\');" type="button" name="accion" value="edit"><i class="fa fa-edit fa-fw"></i></button></td><td><button onclick="deleteUsr(\'usr'+jj[i].idUsuario+'\');" type="button" name="accion" value="delete"><i class="fa fa-times fa-fw"></i></button></td></form></tr>';
           }
+          y+='<tr><form id="newIngreso" onsubmit="event.preventDefault(); handleFormSubmit(this,"createIngreso");"><td></td><td></td><td><input onclick="showIngreso();" type="button" class="w3-btn w3-green w3-xlarge" value="Nuevo"></td></form></tr></tbody>';
           y+="</tbody>";
           document.getElementById("manageUsers").innerHTML=y;
         }else if (h.localeCompare("login")==0) {
@@ -267,6 +266,11 @@ function handleFormSubmit (form,accion) {
           alert("Error al eliminar usuario, intentalo de nuevo");
         }else if (h.localeCompare("updateUsuarioFalse")==0) {
           alert("Error al actualizar tus datos, intentalo de nuevo");
+        }else if (h.localeCompare("createIngreso")==0) {
+          alert("Ingreso agregado correctamente");
+          clearFlujo();
+        }else if (h.localeCompare("createIngresoFalse")==0) {
+          alert("Error al crear el ingreso, intentelo de nuevo");
         }else if(h.localeCompare("registro")==0){
           //console.log("ejecutando registro...");
           alert("¡Usuario registrado exitosamente! Inicia sesión");
@@ -294,8 +298,8 @@ function handleFormSubmit (form,accion) {
   // ...this is where we’d actually do something with the form data...
 };
 function clearFlujo(){
-  document.getElementById("tableIngresos").innerHTML='<tbody><tr class="w3-green"><th>Nombre</th><th>Monto</th><th>Semana de corte</th><th>Periodicidad</th><th></th><th></th></tr></tbody>';
-  document.getElementById("tableEgresos").innerHTML='<tbody><tr class="w3-green"><th>Nombre</th><th>Monto</th><th>Semana de corte</th><th>Periodicidad</th><th></th><th></th></tr></tbody>';
+  document.getElementById("tableIngresos").innerHTML='<tbody><tr class="w3-green"><th>Nombre</th><th>Monto</th><th>Semana de corte</th><th>Periodicidad</th><th></th><th></th></tr>     </tbody>';
+  document.getElementById("tableEgresos").innerHTML='<tbody><tr class="w3-green"><th>Nombre</th><th>Monto</th><th>Semana de corte</th><th>Periodicidad</th><th></th><th></th></tr>     </tbody>';
   handleFormSubmit(document.createElement("form"),"readIngresos");
   handleFormSubmit(document.createElement("form"),"readEgresos");
 }
@@ -304,6 +308,27 @@ function flujoEdit(hola){
 }
 function flujoDelete(hola){
   handleFormSubmit(document.getElementById(hola),'flujoDelete');
+}
+function clearTarjeta(){
+  document.getElementById("tableDebito").innerHTML='<tbody><tr class="w3-green"><th>No de tarjeta</th><th>Fecha de corte</th><th>Saldo</th><th>Comisión fija</th><th>Valor de comisión fija</th><th></th><th></th></tr>     </tbody>';
+  document.getElementById("tableCredito").innerHTML='<tbody><tr class="w3-green"><th>No de tarjeta</th><th>Fecha de corte</th><th>Saldo</th><th>Límite de crédito</th><th>Tasa de interés</th><th></th><th></th></tr>     </tbody>';
+  handleFormSubmit(document.createElement("form"),"readIngresos");
+  handleFormSubmit(document.createElement("form"),"readEgresos");
+}
+function tarjetaDelete(hola){
+  handleFormSubmit(document.getElementById(hola),'deleteTarjeta');
+}
+function tarjetaEdit(hola){
+  handleFormSubmit(document.getElementById(hola),'editTarjeta');
+}
+function showIngreso(){
+  document.getElementById("trNewIngreso").innerHTML='<form id="createIngreso" onsubmit="event.preventDefault(); handleFormSubmit(this,"createEgreso");"></form><td><input class="w3-input" form="createIngreso" type="text" name="nombreFlujo" value=""></td><td><input form="createIngreso" class="w3-input" type="number" name="monto" value=""></td><td><input form="createIngreso" class="w3-input" type="number" name="fecha" value=""></td><td><input form="createIngreso" class="w3-input" type="number" name="periodicidad" value=""> semanas</td><td><button type="button" onclick="mandar(\'createIngreso\')" name="accion" value="crear">Crear</button></td><td></td>';
+}
+function showEgreso(){
+  document.getElementById("trNewEgreso").innerHTML='<form id="createEgreso" onsubmit="event.preventDefault(); handleFormSubmit(this,"createEgreso");"></form><td><input class="w3-input" form="createEgreso" type="text" name="nombreFlujo" value=""></td><td><input form="createEgreso" class="w3-input" type="number" name="monto" value=""></td><td><input form="createEgreso" class="w3-input" type="number" name="fecha" value=""></td><td><input form="createEgreso" class="w3-input" type="number" name="periodicidad" value=""> semanas</td><td><button type="button" onclick="mandar(\'createEgreso\')" name="accion" value="crear">Crear</button></td><td></td>';
+}
+function mandar(hola){
+  handleFormSubmit(document.getElementById(hola),hola);
 }
 
 /*
